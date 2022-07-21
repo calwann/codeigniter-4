9ab9601fa88e7804bab5c095290edc0b09a95f90
Calwann de Souza Freire <calwann.freire@dotgroup.com.br>
2022-07-21 01:46:19 -0300

    feat: add models

---
+++ app/Models/PasswordModel.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class PasswordModel extends Model
+{
+    protected $DBGroup          = 'default';
+    protected $table            = 'passwords';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $insertID         = 0;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = true;
+    protected $protectFields    = true;
+    protected $allowedFields    = [
+        'user_id',
+        'password',
+    ];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+}
